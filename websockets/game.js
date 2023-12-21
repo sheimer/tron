25f134ca86eb468fa81a49c96c2a377b98e8a6d1
sheimer <hidden@debugstube.de>
2023-12-21 19:24:42 +0100

    preparing multi client and later multi games

--- websockets/game.js
+++ websockets/game.js
@@ -1,9 +1,10 @@
-import { WebSocketServer } from 'ws'
+import WebSocket, { WebSocketServer } from 'ws'
 import { randomBytes } from 'crypto'
 
 import { Game } from '../lib/Game.js'
 
 const games = {}
+const key = randomBytes(4).toString('hex')
 
 export const wssGame = new WebSocketServer({
   noServer: true,
@@ -11,21 +12,43 @@ export const wssGame = new WebSocketServer({
   perMessageDeflate: false,
 })
 
+const broadcast = (gameId, message) => {
+  wssGame.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN && client?.gameId === gameId) {
+      client.send(message)
+    }
+  })
+}
+
 wssGame.on('connection', (ws) => {
-  const key = randomBytes(4).toString('hex')
+  ws.gameId = key
+  console.log(
+    'gameId either put to client after game create or set on game connect',
+  )
   ws.on('message', (msg, binary) => {
     msg = JSON.parse(msg.toString())
+    // list games
+    // create game
+    // connectToGame
     if (msg.action === 'init') {
-      games[key] = new Game({
-        size: msg.payload.size,
-        interval: msg.payload.interval,
-        ondraw: (changes) => {
-          ws.send(JSON.stringify({ action: 'draw', key, payload: changes }))
-        },
-        onfinish: (messages) => {
-          ws.send(JSON.stringify({ action: 'finish', key, payload: messages }))
-        },
-      })
+      if (typeof games[key] === 'undefined') {
+        games[key] = new Game({
+          size: msg.payload.size,
+          interval: msg.payload.interval,
+          ondraw: (changes) => {
+            broadcast(
+              key,
+              JSON.stringify({ action: 'draw', key, payload: changes }),
+            )
+          },
+          onfinish: (messages) => {
+            broadcast(
+              key,
+              JSON.stringify({ action: 'finish', key, payload: messages }),
+            )
+          },
+        })
+      }
       ws.send(JSON.stringify({ action: 'setState', payload: 'connected' }))
     } else if (msg.action === 'setPlayers') {
       games[key].setPlayers(msg.payload)
