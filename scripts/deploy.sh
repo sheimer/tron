658fa1286128e2e68a047f94225ebe369fb21cfb
sheimer <hidden@debugstube.de>
2025-07-31 18:18:48 +0200

    "caching" node_modules

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,28 +9,33 @@ fi
 
 path="/srv/tron"
 
+INFO='\033[0;36m'
+NC='\033[0m' # No Color
+
+echo -e "${INFO}*** rsync${NC}"
+
 rsync --delete-after --filter=":e- .gitignore" --filter "- .git/" -v -a . $target:$path/deploy
 
 ssh $target bash <<EOF
 cd $path
-pwd
 
-echo
-echo "*** copy deploy to build"
-echo
+mkdir -p run/node_modules
+
+echo -e "${INFO}*** copy deploy to build${NC}"
 cp -a deploy build
+echo -e "${INFO}*** copy node_modules to build${NC}"
+cp -a run/node_modules build/
 cd build
 
-echo
-echo "*** npm install"
-echo
+echo -e "${INFO}*** npm install${NC}"
 npm install
 cd ..
 
-echo
-echo "*** restart service"
-echo
-sudo systemctl stop tron.service && rm -rf run
+echo -e "${INFO}*** restart service${NC}"
+sudo systemctl stop tron.service
+rm -rf run
 mv build run
 sudo systemctl start tron.service
+
+echo -e "${INFO}*** done${NC}"
 EOF
